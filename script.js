355a445a04589d9223d57834e5fff486f88e3197
Alexrocha666 <alexpinposo@gmail.com>
2025-03-22 23:23:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-function loadMovie(src) {
-  const player = document.getElementById('player');
-  player.src = src;
-  player.play();
+async function sendMessage() {
+    let userInput = document.getElementById("user-input").value;
+    if (!userInput) return;
+
+    let chatBox = document.getElementById("chat-box");
+    chatBox.innerHTML += `<div><b>Você:</b> ${userInput}</div>`;
+
+    document.getElementById("user-input").value = "";
+
+    let response = await fetch(`https://api.monkedev.com/fun/chat?msg=${encodeURIComponent(userInput)}`);
+    let data = await response.json();
+
+    chatBox.innerHTML += `<div><b>Bot:</b> ${data.response}</div>`;
+    chatBox.scrollTop = chatBox.scrollHeight;
 }
